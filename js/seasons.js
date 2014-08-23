bdc6c8a2ad1169078902d0ba2fde87ce5bf8cd9a
Erik Lantto <lantto@gmail.com>
2014-08-23 20:46:12 +0200

    Refactor

--- js/seasons.js
+++ js/seasons.js
@@ -9,10 +9,11 @@ var utils = {
 };
 
 var config = {
-    logicTimer: 2000,
-    pool: 10000,
-    baseValue: 2500,
-    degeneration: 120
+    logicTimer: 500,
+    pool: [7500, 7500, 5000, 5000, 2500, 2500],
+    baseValue: 0,
+    maxValue: 10000,
+    degeneration: 0
 };
 
 var nature = new Vue({
@@ -30,30 +31,38 @@ var Village = (function() {
         
         this.modifier = modifier;
         
-        this.energy = config.baseValue;
-        this.food = config.baseValue;
-        this.water = config.baseValue;
-        this.happiness = config.baseValue;
-        this.chill = config.baseValue;
-        this.heat = config.baseValue;
+        this.energy
+        = this.food
+        = this.water
+        = this.happiness
+        = this.chill
+        = this.heat 
+        = config.baseValue;
         
         var resources = ['energy', 'food', 'water', 'happiness', 'chill', 'heat'];
-        var pool = config.pool;
+        
+        var pool = config.pool.slice(0);
         
         while (resources.length > 0) {
-            var index = utils.random(0, resources.length - 1);
-            var add = utils.random(0, pool);
-            this[resources[index]] += add;
-            pool -= add;
-            resources.splice(index, 1);
+            var resourceIndex = utils.random(0, resources.length - 1);
+            var poolIndex = utils.random(0, pool.length - 1);
+            this[resources[resourceIndex]] += pool[poolIndex];
+            resources.splice(resourceIndex, 1);
+            pool.splice(poolIndex, 1);
         }
     }
 
     Village.prototype.update = function() {
         var solstice = parseInt(nature.$data.solstice) * this.modifier,
             weather = parseInt(nature.$data.weather) * this.modifier,
-            season = parseInt(nature.$data.season) * this.modifier;
-        
+            season = parseInt(nature.$data.season) * this.modifier,
+            energyAlter = this.getIncrementValue(-solstice),
+            foodAlter = this.getIncrementValue(solstice),
+            waterAlter = this.getIncrementValue(weather),
+            happinessAlter = this.getIncrementValue(-weather),
+            chillAlter = this.getIncrementValue(season),
+            heatAlter = this.getIncrementValue(-season);
+
         /****************
          *   SOL WEA SEA
          * E  -         
@@ -63,18 +72,27 @@ var Village = (function() {
          * C          +
          * H          -
          ****************/
-            
-        this.energy += -(solstice) - config.degeneration;
-        this.food += solstice - config.degeneration;
-        this.water += weather - config.degeneration;
-        this.happiness += -(weather) - config.degeneration;
-        this.chill += season - config.degeneration;
-        this.heat += -(season) - config.degenration;
         
+        this.energy += energyAlter;
+        this.food += foodAlter;
+        this.water += waterAlter;
+        this.happiness += happinessAlter;
+        this.chill += chillAlter;
+        this.heat += heatAlter;
+        
+        if (this.energy > config.maxValue) this.energy = config.maxValue;
+        if (this.food > config.maxValue) this.food = config.maxValue;
+        if (this.water > config.maxValue) this.water = config.maxValue;
+        if (this.happiness > config.maxValue) this.happiness = config.maxValue;
+        if (this.chill > config.maxValue) this.chill = config.maxValue;
+        if (this.heat > config.maxValue) this.heat = config.maxValue;
         
         if (this.energy <= 0
             || this.food <= 0
             || this.water <= 0
+            || this.happiness <= 0
+            || this.chill <= 0
+            || this.heat <= 0
         ) {
             console.log(this.id + 'ern village died');
         }
@@ -82,10 +100,20 @@ var Village = (function() {
         $('#' + this.id + ' .energy').html(this.energy);
         $('#' + this.id + ' .food').html(this.food);
         $('#' + this.id + ' .water').html(this.water);
+        $('#' + this.id + ' .happiness').html(this.happiness);
+        $('#' + this.id + ' .chill').html(this.chill);
+        $('#' + this.id + ' .heat').html(this.heat);
         
         $('#' + this.id + ' .energy-alter').html(energyAlter);
         $('#' + this.id + ' .food-alter').html(foodAlter);
         $('#' + this.id + ' .water-alter').html(waterAlter);
+        $('#' + this.id + ' .happiness-alter').html(happinessAlter);
+        $('#' + this.id + ' .chill-alter').html(chillAlter);
+        $('#' + this.id + ' .heat-alter').html(heatAlter);
+    };
+    
+    Village.prototype.getIncrementValue = function(element) {
+        return 50 + element / 2 - config.degeneration;
     };
 
     return Village;
